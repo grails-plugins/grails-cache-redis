7b489672ee7a547b2202a990ca4bbc60e1a90960
Burt Beckwith <beckwithb@vmware.com>
2012-07-04 20:04:08 -0400

    1.0.0 release

--- CacheRedisGrailsPlugin.groovy
+++ CacheRedisGrailsPlugin.groovy
@@ -36,7 +36,7 @@ class CacheRedisGrailsPlugin {
 
 	private final Logger log = LoggerFactory.getLogger('grails.plugin.cache.CacheRedisGrailsPlugin')
 
-	String version = '1.0.0.BUILD-SNAPSHOT'
+	String version = '1.0.0'
 	String grailsVersion = '2.0 > *'
 	def loadAfter = ['cache']
 	def pluginExcludes = [
