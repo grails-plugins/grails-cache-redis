d757006f0a793b01cc603d494ecca29b69663c3c
Colin Harrington <colin.harrington@gmail.com>
2016-06-16 15:14:04 -0500

    Formatting in tests

--- src/test/groovy/grails/plugin/cache/redis/GrailsRedisCacheManagerSpec.groovy
+++ src/test/groovy/grails/plugin/cache/redis/GrailsRedisCacheManagerSpec.groovy
@@ -7,31 +7,31 @@ class GrailsRedisCacheManagerSpec extends Specification {
 
     def 'it should default expiration to never expire for all created caches when creating a cache manager with a null ttl value.'() {
         given:
-        Long ttl = null
-        RedisTemplate template = Mock(RedisTemplate)
-        GrailsRedisCacheManager cacheManager = new GrailsRedisCacheManager(template)
+            Long ttl = null
+            RedisTemplate template = Mock(RedisTemplate)
+            GrailsRedisCacheManager cacheManager = new GrailsRedisCacheManager(template)
 
-        when: cacheManager.setTimeToLive(ttl)
-
-        and: GrailsRedisCache cache = cacheManager.getCache('book')
+        when:
+                cacheManager.setTimeToLive(ttl)
+        and:
+                GrailsRedisCache cache = cacheManager.getCache('book')
 
         then:
-
-        assert cache.ttl == GrailsRedisCache.NEVER_EXPIRE
+            assert cache.ttl == GrailsRedisCache.NEVER_EXPIRE
     }
 
     def 'it should set expiration on all caches with configured ttl when creating a cache manager.'() {
         given:
-        Long ttl = 5
-        RedisTemplate template = Mock(RedisTemplate)
-        GrailsRedisCacheManager cacheManager = new GrailsRedisCacheManager(template)
+            Long ttl = 5
+            RedisTemplate template = Mock(RedisTemplate)
+            GrailsRedisCacheManager cacheManager = new GrailsRedisCacheManager(template)
 
-        when: cacheManager.setTimeToLive(ttl)
-
-        and: GrailsRedisCache cache = cacheManager.getCache('book')
+        when:
+                cacheManager.setTimeToLive(ttl)
+        and:
+            GrailsRedisCache cache = cacheManager.getCache('book')
 
         then:
-
-        assert cache.ttl == ttl
+            assert cache.ttl == ttl
     }
 }
--- src/test/groovy/grails/plugin/cache/redis/GrailsRedisCacheSpec.groovy
+++ src/test/groovy/grails/plugin/cache/redis/GrailsRedisCacheSpec.groovy
@@ -9,32 +9,32 @@ class GrailsRedisCacheSpec extends Specification {
 
     def 'it should default expiration to never expire when creating a cache with a null ttl value.'() {
         given:
-        RedisCachePrefix cachePrefix = new DefaultRedisCachePrefix()
-        String cacheName = 'book'
-        byte[] prefix = cachePrefix.prefix(cacheName)
-        RedisTemplate template = Mock(RedisTemplate)
-        Long ttl = null
+            RedisCachePrefix cachePrefix = new DefaultRedisCachePrefix()
+            String cacheName = 'book'
+            byte[] prefix = cachePrefix.prefix(cacheName)
+            RedisTemplate template = Mock(RedisTemplate)
+            Long ttl = null
 
-        when: GrailsRedisCache cache = new GrailsRedisCache(cacheName, prefix, template, ttl)
+        when:
+            GrailsRedisCache cache = new GrailsRedisCache(cacheName, prefix, template, ttl)
 
         then:
-
-        assert cache.ttl == GrailsRedisCache.NEVER_EXPIRE
+            assert cache.ttl == GrailsRedisCache.NEVER_EXPIRE
     }
 
     def 'it should set expiration with configured ttl when creating a cache.'() {
         given:
-        RedisCachePrefix cachePrefix = new DefaultRedisCachePrefix()
-        String cacheName = 'book'
-        byte[] prefix = cachePrefix.prefix(cacheName)
-        RedisTemplate template = Mock(RedisTemplate)
-        Long ttl = 500
+            RedisCachePrefix cachePrefix = new DefaultRedisCachePrefix()
+            String cacheName = 'book'
+            byte[] prefix = cachePrefix.prefix(cacheName)
+            RedisTemplate template = Mock(RedisTemplate)
+            Long ttl = 500
 
-        when: GrailsRedisCache cache = new GrailsRedisCache(cacheName, prefix, template, ttl)
+        when:
+            GrailsRedisCache cache = new GrailsRedisCache(cacheName, prefix, template, ttl)
 
         then:
-
-        assert cache.ttl == ttl
+            assert cache.ttl == ttl
     }
 
 
