d1092913aa4bb9a535f95156e8d9a4ad92f3edf3
sbglasius <sbglasius@users.noreply.github.com>
2025-06-12 10:19:30 +0000

    Deploying to gh-pages - 10:19:30

--- snapshot/javadoc/package-search-index.js
+++ snapshot/javadoc/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"grails.plugin.cache.redis"},{"l":"grails.plugin.cache.redis.internal"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"grails.plugin.cache.redis"},{"l":"grails.plugin.cache.redis.internal"}]
